7f24144e5ea1060f2b1d5bdceb2a39d3811fa0bc
leechance <workinterface@126.com>
2015-12-16 15:23:43 +0800

    first commit

---
+++ 唯品会网络请求刷新动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  唯品会网络请求刷新动画
+//
+//  Created by Honzon-0 on 15/11/26.
+//  Copyright © 2015年 Honzon-0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
